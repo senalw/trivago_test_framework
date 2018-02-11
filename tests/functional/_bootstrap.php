2460b9aadc6f25eb0e033809b9b410b856a94000
Senal Weerasinghe <senalw@millenniumit.com>
2018-02-11 12:26:55 +0530

    edited readme.md and codeception.yml

---
+++ tests/functional/_bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: senalw
+ * Date: 2/5/2018
+ * Time: 10:01 AM
+ */
+
+include_once __DIR__ . '\_support\Helper\TestListener.php';
+include_once __DIR__ . '\_support\Helper\AcceptanceHelper.php';
+
